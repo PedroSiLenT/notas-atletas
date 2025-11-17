0e55ddbcb4410805e36eb1d04ad1868328d96e72
Pedro Henrique <140028695+PedroSiLenT@users.noreply.github.com>
2025-11-17 19:33:46 -0300

    Update dados-atletas.js

--- main/dados-atletas.js
+++ main/dados-atletas.js
@@ -10,7 +10,7 @@ class Atleta {
     }
 
 
-    calculaCatecoria() {
+    calculaCategoria() {
         if(this.idade >= 9 && this.idade <= 11) {
             return "Infantil"
         } else if(this.idade <= 13) {
@@ -33,12 +33,60 @@ class Atleta {
 
 
     calculaMediaValida() {
-        
+        this.notas = this.notas.sort((a, b) => a - b);
+
+
+        let notasComputadas = this.notas.slice(1, 4);
+
+        let soma = 0;
+        notasComputadas.forEach(function(nota) {
+        soma += nota;
+        });
+
+        let media = soma / notasComputadas.length;
+
+        return media
+    }
+
+
+    obtemNomeAtleta() {
+        return this.nome
+    }
+
+
+    obtemIdadeAtleta() {
+        return this.idade
+    }
+
+
+    obtemPesoAtleta(){
+        return this.peso
+    }
+
+
+    obtemNotasAtleta() {
+        return this.notas
+    }
+
+
+    obtemAltura() {
+        return this.altura
     }
 }
 
 
 
-let atleta1 = new Atleta("João", "Silva", 12, 40, 1.50, [10, 9, 8]);
 
-console.log(atleta1.calcularIMC());
+let atleta = new Atleta("João", "Silva", 18, 40, 1.70, [10, 9.34, 8.42, 10, 7.88]);
+
+console.log(
+  `Nome: ${atleta.obtemNomeAtleta()}\n` +
+  `Idade: ${atleta.obtemIdadeAtleta()}\n` +
+  `Peso: ${atleta.obtemPesoAtleta()}\n` +
+  `Altura: ${atleta.obtemAltura()}\n` +
+  `Notas: ${atleta.obtemNotasAtleta()}\n` +
+  `Categoria: ${atleta.calculaCategoria()}\n` +
+  `IMC: ${atleta.calcularIMC()}\n` +
+  `Media Valida: ${atleta.calculaMediaValida()}`
+
+);
